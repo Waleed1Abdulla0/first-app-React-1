b7fcbddfa7531c56485979a8084c5459c9a73ba1
Waleed1Abdulla0 <waleedoh9995@gmail.com>
2023-12-12 02:58:33 +0300

    New ver

---
+++ src/compo/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function footer() {
+  return (
+    <div>
+      <footer>
+        Designed and developed by Waleed Senan
+        <span>🧡</span>
+      </footer>
+    </div>
+  );
+}
